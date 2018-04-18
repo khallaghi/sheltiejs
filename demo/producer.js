14ade47680a6271aad2d5f4b65264979a0d749bf
Mohammad Khallaghi <md.khallaghi@gmail.com>
2018-04-18 18:05:26 +0430

    add sample run command

--- demo/producer.js
+++ demo/producer.js
@@ -18,3 +18,38 @@ var message = process.argv.slice(2).join(' ') ||
 	'info: Hello World!';
 producer(message);
 
+
+/*
+
+sample command
+
+
+node producer.js '{"name": "video-ftp", "action":"create", "namespace":"default", "id": "7", "args":
+[
+{
+    "key": "z",
+    "value": "0"
+},
+{
+    "key": "t",
+    "value": "00:00:10"
+},
+{
+    "key": "o",
+    "value": "/CORE/proxy/all/vod2/www/files/ahmadreza.khattokhat.com/0ea8c195-81df-4ccf-b130-3dd01ef0c76a/hd.mp4"
+},
+{
+    "key": "s",
+    "value": "/CORE/proxy/all/vod2/www/files/ahmadreza.khattokhat.com/0ea8c195-81df-4ccf-b130-3dd01ef0c76a/thumbnail.json"
+},
+{
+    "key": "d",
+    "value": "/CORE/proxy/all/vod2/www/files/ahmadreza.khattokhat.com/0ea8c195-81df-4ccf-b130-3dd01ef0c76a/thumbnail.json"
+},
+{
+    "key": "e",
+    "value": "/CORE/proxy/all/vod2/www/files/ahmadreza.khattokhat.com/0ea8c195-81df-4ccf-b130-3dd01ef0c76a/tooltip.png"
+}
+]
+}'
+*/
